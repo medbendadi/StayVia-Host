8e06d795bd77a3335de3b91c70ebcfe924fb16de
medbendadi <itsbendadi@gmail.com>
2025-12-01 10:25:52 -0500

    change logo

--- src/components/Layout/Header/BrandLogo/Logo.tsx
+++ src/components/Layout/Header/BrandLogo/Logo.tsx
@@ -4,7 +4,7 @@ const Logo: React.FC = () => {
   return (
     <>
       <Image
-        src={'/images/header/dark-logo.svg'}
+        src={'/images/header/logo.svg'}
         alt='logo'
         width={150}
         height={68}
@@ -12,7 +12,7 @@ const Logo: React.FC = () => {
         className='dark:hidden'
       />
       <Image
-        src={'/images/header/logo.svg'}
+        src={'/images/header/dark-logo.svg'}
         alt='logo'
         width={150}
         height={68}
